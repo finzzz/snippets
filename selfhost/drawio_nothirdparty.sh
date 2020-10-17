80e397d43f13588ccb45e609288182b3ae5af285
Vincent Carlos <gitlab@finzzz.net>
2020-10-17 04:48:29 +0000

    Upload New File

---
+++ selfhost/drawio_nothirdparty.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# download release
+VERSION=$(curl -s https://github.com/jgraph/drawio/releases/latest/ | egrep -o "tag/.*[0-9]" | cut -d '/' -f 2)
+wget -O - https://github.com/jgraph/drawio/archive/"$VERSION".tar.gz | tar -xzf -
+
+FOLDERNAME="drawio-"${VERSION:1}""
+JS_FILE=""$FOLDERNAME"/src/main/webapp/js/app.min.js"
+
+cp "$JS_FILE" "$JS_FILE".bak
+sed -ie "s#https://apis.google.com/#notexist/#" "$JS_FILE"
+sed -ie "s#https://app.diagrams.net#notexist/#" "$JS_FILE"
+sed -ie "s#https://api.trello.com/#notexist/#" "$JS_FILE"
+sed -ie "s#https://api.trello.com/#notexist/#" "$JS_FILE"
+sed -ie "s#https://code.jquery.com/#notexist/#" "$JS_FILE" # trello jquery url
+sed -ie "s#https://www.dropbox.com/#notexist/#" "$JS_FILE"
+
